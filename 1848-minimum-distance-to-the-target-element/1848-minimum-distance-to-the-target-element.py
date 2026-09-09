c12ccc43c51a99af96fd417caaa1081f5c54b11a
Subha Karmakar <169050267+SSubha16@users.noreply.github.com>
2026-09-09 21:44:08 +0530

    Time: 3 ms (18.72%), Space: 19.5 MB (16.24%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -1,9 +1,3 @@
 class Solution:
     def getMinDistance(self, nums: list[int], target: int, start: int) -> int:
-        min_dist = float("inf")
-        for i, val in enumerate(nums):
-            if val == target:
-                min_dist = min(min_dist, abs(i - start))
-                if min_dist == 0:
-                    return 0
-        return min_dist
+        return min(abs(i - start) for i, val in enumerate(nums) if val == target)
